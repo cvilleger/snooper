cc5e740be40319d13e86ed407d7c7d3afb2ea1c3
Christophe Villeger <villeger.c@gmail.com>
2018-10-25 01:36:46 +0200

    init

--- src/Controller/RepositoryController.php
+++ src/Controller/RepositoryController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class RepositoryController extends AbstractController
+class RepositoryController extends Controller
 {
     public function index()
     {
-        return $this->render('repository/index.html.twig');
+        $client = $this->get('eight_points_guzzle.client.github_client');
+        $res = $client->get($this->getUser()->getReposUrl());
+        $repos = json_decode($res->getBody()->getContents());
+        return $this->render('repository/index.html.twig', [
+            'repos' => $repos,
+        ]);
     }
 }
